a758e17762d85300bbd92c7f50a543fd7043b0e3
CSharpFri <CSharpFri@CLASS-2-02>
2017-11-10 18:39:33 +0100

    Init commit

---
+++ Zvierata2/Zviera.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Zvierata
+{
+    abstract class Zviera
+    {
+        protected string rasa, farba;
+        protected int vek;
+        protected float vaha;
+        private bool velke;
+        private int pocetNoh;
+        protected bool nakrmeno;
+
+        public Zviera(string rasa, string farba, int vek, float vaha, bool velke, int pocetNoh)
+        {
+            this.rasa = rasa;
+            this.farba = farba;
+            this.vek = vek;
+            this.vaha = vaha;
+            this.velke = velke;
+            this.pocetNoh = pocetNoh;
+        }
+
+        public void VypisVlastnosti()
+        {
+            Console.WriteLine("Zviera ma tieto vlastnosti:\nrasa: {0}\nfarba: {1}\nvek: {2}\nvaha: {3}\nzviera je velke: {4}\npocet noh: {5}\n",
+                rasa, farba, vek, vaha, velke, pocetNoh);
+        }
+
+        public void VydajZvuk()
+        {
+            Console.WriteLine("Zvuk zvierata:\nToto je vseobecny zvuk zvierata.\n");
+        }
+        public abstract void NakrmZviera();
+    }
+}
